a0223495c936f98422767f83cb9f75bca42e6f7f
Blake Mackey <blakemackey015@gmail.com>
2025-05-24 20:23:25 -0400

    add attacking / recieving damages functions

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,6 +13,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] protected PlayerController player;
     [SerializeField] protected float speed;
 
+    [SerializeField] protected float damage;
+
     protected float recoilTimer;
     protected Rigidbody2D rb;
 
@@ -55,5 +57,17 @@ public class Enemy : MonoBehaviour
             rb.AddForce(-_hitForce * recoilFactor * _hitDirection);
             isRecoiling = true;
         }
+    }
+
+    protected void OnTriggerStay2D(Collider2D _other)
+    {
+        if (_other.CompareTag("Player") && !PlayerController.Instance.pState.invincible)
+        {
+        }
+    }
+
+    protected virtual void Attack()
+    {
+        PlayerController.Instance.TakeDamage(damage);
     }
 }
